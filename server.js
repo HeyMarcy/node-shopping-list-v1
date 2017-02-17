e3cd283b7da3e3a11af239b2ebcc7fbca37d9952
marcy <marcy.montross@gmail.com>
2017-02-17 10:49:53 -0600

    Created 'Delete' endpoints

--- server.js
+++ server.js
@@ -44,11 +44,16 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+
 app.post('/recipes', jsonParser, (req, res) => {
   // ensure `name` and `ingredients` are in request body
-
   const requiredFields = ['name', 'ingredients'];
-  
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -62,7 +67,11 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipes item \`${req.params.id}\``);
+  res.status(204).end();
+});
 
 
 app.get('/recipes', (req, res) => {
